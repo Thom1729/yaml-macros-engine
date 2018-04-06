e648cae23623d906acdd450791d62f61bab187c5
thom <thom@localhost.localdomain>
2018-04-06 16:32:58 -0400

    Improved flexibility of printing.

--- st3/yamlmacros/src/build.py
+++ st3/yamlmacros/src/build.py
@@ -6,35 +6,36 @@ from .engine import process_macros
 from .yaml_provider import get_yaml_instance
 from .engine import MacroError
 
-def build(source_text, destination_path, error_stream, arguments, error_highlighter):
+def build(source_text, destination_path, error_stream, arguments, error_highlighter=None):
     t0 = time.perf_counter()
 
-    error_stream.print('Building %s... (%s)' % (path.basename(arguments['file_path']), arguments['file_path']))
+    error_stream.write('Building %s... (%s)\n' % (path.basename(arguments['file_path']), arguments['file_path']))
 
     def done(message):
-        error_stream.print('[{message} in {time:.2f} seconds.]\n'.format(
+        error_stream.write('[{message} in {time:.2f} seconds.]\n\n'.format(
             message=message,
             time = time.perf_counter() - t0
         ))
 
     def handle_error(e):
         if isinstance(e, MacroError):
-            error_stream.print()
-            error_stream.print(e.message)
-            error_stream.print(str(e.node.start_mark))
+            error_stream.write('\n')
+            error_stream.write(e.message + '\n')
+            error_stream.write(str(e.node.start_mark) + '\n')
 
             if e.__cause__:
                 handle_error(e.__cause__)
 
-            error_highlighter.highlight(
-                e.context.get('file_path'),
-                e.node.start_mark.index,
-                e.node.end_mark.index,
-                e.message,
-            )
+            if error_highlighter:
+                error_highlighter.highlight(
+                    e.context.get('file_path'),
+                    e.node.start_mark.index,
+                    e.node.end_mark.index,
+                    e.message,
+                )
         else:
-            error_stream.print()
-            error_stream.print(''.join(traceback.format_exception(None, e, e.__traceback__)))
+            error_stream.write('\n')
+            error_stream.write(''.join(traceback.format_exception(None, e, e.__traceback__)) + '\n')
 
     try:
         result = process_macros(source_text, arguments=arguments)
@@ -47,5 +48,5 @@ def build(source_text, destination_path, error_stream, arguments, error_highligh
 
     with open(destination_path, 'w') as output_file:
         serializer.dump(result, stream=output_file)
-        error_stream.print('Compiled to %s. (%s)' % (path.basename(destination_path), destination_path))
+        error_stream.write('Compiled to %s. (%s)\n' % (path.basename(destination_path), destination_path))
         done('Succeeded')
